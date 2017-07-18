af30f29245550184700945a4c9fc12364745bb0a
Ross Zurowski <ross@rosszurowski.com>
2017-07-18 10:02:26 -0700

    Fix build config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   exportPathMap: () => ({
-    '/': { page: '/' },
-    '/2017/japan': { page: '/2017/japan' },
-    '/100': { page: '/100' },
-    // NOTE: we manually move this exported file to 404.html in the build process.
-    // Once Next allows exporting a plain .html file, let's change this.
-    '/_error': { page: '_error' },
+    '/': { page: '/' }
   }),
 }
